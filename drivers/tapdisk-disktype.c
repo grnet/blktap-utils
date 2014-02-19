82f146151fc5d2cff8cec3bc742e8779edd1f799
Chrysostomos Nanakos <cnanakos@grnet.gr>
2014-02-19 17:57:02 +0200

    Add Archipelago disk type

--- drivers/tapdisk-disktype.c
+++ drivers/tapdisk-disktype.c
@@ -127,6 +127,12 @@ static const disk_info_t valve_disk = {
        DISK_TYPE_FILTER,
 };
 
+static const disk_info_t archipelago_disk = {
+    "archipelago",
+    "Archipelago volume image",
+    0,
+};
+
 const disk_info_t *tapdisk_disk_types[] = {
 	[DISK_TYPE_AIO]	= &aio_disk,
 	[DISK_TYPE_SYNC]	= &sync_disk,
@@ -143,6 +149,7 @@ const disk_info_t *tapdisk_disk_types[] = {
 	[DISK_TYPE_VALVE]       = &valve_disk,
 	[DISK_TYPE_LLPCACHE]    = &llpcache_disk,
 	[DISK_TYPE_LLECACHE]    = &llecache_disk,
+	[DISK_TYPE_ARCHIPELAGO]    = &archipelago_disk,
 	0,
 };
 
@@ -167,6 +174,8 @@ extern struct tap_disk tapdisk_llpcache;
 extern struct tap_disk tapdisk_llecache;
 extern struct tap_disk tapdisk_valve;
 
+extern struct tap_disk tapdisk_archipelago;
+
 const struct tap_disk *tapdisk_disk_drivers[] = {
 	[DISK_TYPE_AIO]         = &tapdisk_aio,
 #if 0
@@ -188,6 +197,7 @@ const struct tap_disk *tapdisk_disk_drivers[] = {
 	[DISK_TYPE_LLPCACHE]    = &tapdisk_llpcache,
 	[DISK_TYPE_LLECACHE]    = &tapdisk_llecache,
 	[DISK_TYPE_VALVE]       = &tapdisk_valve,
+	[DISK_TYPE_ARCHIPELAGO] = &tapdisk_archipelago,
 	0,
 };
 
